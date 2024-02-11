3ac75f27a5b1ef27fb305625d05cee62744200bc
jkengs <107338480+jkengs@users.noreply.github.com>
2024-02-11 11:57:28 +1100

    feat: connect upload button to endpoint

--- frontend/src/components/Buttons/UploadButton.jsx
+++ frontend/src/components/Buttons/UploadButton.jsx
@@ -1,19 +1,64 @@
+import { useRef } from 'react';
 import { List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import UploadFileIcon from '@mui/icons-material/UploadFile';
 
 const UploadButton = () => {
-    return (
-        <List>
-          <ListItem  disablePadding>
-            <ListItemButton>
-              <ListItemIcon style={{minWidth: '40px'}}>
-                <UploadFileIcon />
-              </ListItemIcon>
-              <ListItemText primary="Upload" />
-            </ListItemButton>
-          </ListItem>
-        </List>
-    )
+  const fileInputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    // Trigger the click event of the hidden file input
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = async (event) => {
+    // Handle the file selection here
+    const selectedFile = event.target.files[0];
+    // console.log('Selected file:', selectedFile);
+
+    if (selectedFile) {
+      try {
+        const formData = new FormData();
+        formData.append('file', selectedFile);
+
+        // TODO: Replace endpoint URL
+        const response = await fetch('http://localhost:9999/upload', {
+          method: 'POST',
+          body: formData,
+        });
+
+        if (response.ok) {
+          const responseData = await response.json();
+          // console.log('Response data:', responseData);
+          localStorage.setItem('uploadedData', JSON.stringify(responseData));
+          alert('File uploaded successfully!')
+        }
+      } catch (error) {
+        alert('Error uploading file! Try again!');
+      }
+    } else {
+      alert('No file was selected.');
+    }
+  };
+
+  return (
+    <List>
+      <ListItem disablePadding>
+        <ListItemButton onClick={handleButtonClick}>
+          <ListItemIcon style={{ minWidth: '40px' }}>
+            <UploadFileIcon />
+          </ListItemIcon>
+          <ListItemText primary="Upload" />
+        </ListItemButton>
+      </ListItem>
+      {/* Hidden file input */}
+      <input
+        ref={fileInputRef}
+        type="file"
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
+      />
+    </List>
+  );
 };
 
 export default UploadButton;
