e80fa29e28d580513b72938a7af299a5cc875f46
Michael Milton <michael.r.milton@gmail.com>
2024-03-25 12:11:21 +1100

    Clean up the parameter form

--- frontend/src/components/Form/Parameters.tsx
+++ frontend/src/components/Form/Parameters.tsx
@@ -1,23 +1,9 @@
 import * as React from "react";
-import { useForm, SubmitHandler, Controller, FormProvider, useFormContext } from "react-hook-form"
-import type { Parameters } from "../../client"
-import { Tooltip } from "@mui/material";
-import Slider from "../Slider/Slider"
+import Slider from "../Slider/Slider";
 
 export default function ParameterForm() {
   return (
       <form>
-        {/* <Tooltip title="Median time to division destiny" >
-          <Controller
-            name="mDD"
-            control={control}
-            render={({ field }) => <Slider
-              label="mdd"
-              {...field}
-              />}
-          ></Controller>
-          { <SliderElement max={100} label="mDD" label="mDD")}/> }
-        </Tooltip> */}
         <Slider label="mDD"/>
         <Slider label="sDD"/>
         <Slider label="mDie"/>
@@ -28,7 +14,6 @@ export default function ParameterForm() {
         <Slider label="sUns"/>
         <Slider label="b"/>
         <Slider label="p"/> 
-        <input type="submit" />
       </form>
   )
 }
